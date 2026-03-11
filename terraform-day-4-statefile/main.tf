06c70d08a0c183237f0e4963904f26c4d8168e21
Mandeep <mandeeppathania333@gmail.com>
2026-03-11 09:51:55 +0530

    day-4

---
+++ terraform-day-4-statefile/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "Mandu-VPC"
+    }
+  
+}
+resource "aws_instance" "name" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "Mandu-Instance"
+    }
+  
+}
