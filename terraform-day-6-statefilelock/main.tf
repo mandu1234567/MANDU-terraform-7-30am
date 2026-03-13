ff620c6349d250b141fe4ab7bc7fde6187652c5b
Mandeep <mandeeppathania333@gmail.com>
2026-03-13 09:39:23 +0530

    day-6

---
+++ terraform-day-6-statefilelock/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "Mandu_vpc"
+    }
+  
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name = "Mandu_vpc_subnet"
+    }
+}
