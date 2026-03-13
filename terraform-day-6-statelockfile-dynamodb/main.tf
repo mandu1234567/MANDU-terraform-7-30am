5f08d8020fdf113700cb52192c9c4da058db7449
Mandeep <mandeeppathania333@gmail.com>
2026-03-13 21:49:46 +0530

    day-6

---
+++ terraform-day-6-statelockfile-dynamodb/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "Mandu_vpc"
+    }
+  
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name = "Mandu_vpc_subnet"
+    }
+}
+
+resource "aws_subnet" "name-2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+      Name = "Mandu_vpc_subnet-2"
+    }
+} 
